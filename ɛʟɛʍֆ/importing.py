ccb951286a78f46d817a77162ea8f70c21357247
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-21 15:52:27 +0530

    qwd

--- ɛʟɛʍֆ/importing.py
+++ ɛʟɛʍֆ/importing.py
@@ -13,155 +13,143 @@ from PIL import Image
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from ʏօʊȶʊɮɛʟɨ import *
-os.system(KK)
+
 DOWNLOAD_LOCATION = "./Downloads"
-from dlhunt.dlhunt import *
+
 
 
 @Client.on_callback_query()
-async def catch_youtube_fmtid(
-    _,
-    buta):
-    fetch_elems = buta.data
-    if fetch_elems.startswith("ytdata||"):
-        fetchedlink = fetch_elems.split("||")[-1]
-        format_id = fetch_elems.split("||")[-2]
-        media_type = fetch_elems.split("||")[-3].strip()
+async def catch_youtube_fmtid(c, m):
+    cb_data = m.data
+    if cb_data.startswith("ytdata||"):
+        yturl = cb_data.split("||")[-1]
+        format_id = cb_data.split("||")[-2]
+        media_type = cb_data.split("||")[-3].strip()
         print(media_type)
         if media_type == 'audio':
-            pod = InlineKeyboardMarkup(
-                [[InlineKeyboardButton(
-                "Best-Mp3",
-                callback_data=f"{media_type}||{format_id}||{fetchedlink}")]])
+            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
+                "Audio", callback_data=f"{media_type}||{format_id}||{yturl}"),]])
         else:
-            pod = InlineKeyboardMarkup(
-                [[InlineKeyboardButton(
-                "Best-Mp4",
-                callback_data=f"{media_type}||{format_id}||{fetchedlink}")]])
-        await buta.edit_message_reply_markup(pod)
+            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
+                "Video", callback_data=f"{media_type}||{format_id}||{yturl}")]])
+
+        await m.edit_message_reply_markup(buttons)
+
     else:
         raise ContinuePropagation
 
 @Client.on_callback_query()
-async def catch_youtube_dldata(
-    fetch,
-    data_pod):
-    fetch_elems = data_pod.data.strip()
-    fetchedlink = fetch_elems.split("||")[-1]
-    format_id = fetch_elems.split("||")[-2]
-    urljpegclone = "/app/downloads" + \
-        "/" + str(data_pod.message.chat.id) + ".jpeg"
-    print(urljpegclone)
-    if os.path.exists(urljpegclone):
+async def catch_youtube_dldata(c, q):
+    cb_data = q.data.strip()
+    #print(q.message.chat.id)
+    # Callback Data Check
+    yturl = cb_data.split("||")[-1]
+    format_id = cb_data.split("||")[-2]
+    thumb_image_path = "/app/downloads" + \
+        "/" + str(q.message.chat.id) + ".jpg"
+    print(thumb_image_path)
+    if os.path.exists(thumb_image_path):
         width = 0
         height = 0
-        metadata = extractMetadata(createParser(urljpegclone))
+        metadata = extractMetadata(createParser(thumb_image_path))
+        #print(metadata)
         if metadata.has("width"):
             width = metadata.get("width")
         if metadata.has("height"):
             height = metadata.get("height")
-        img = Image.open(urljpegclone)
-        if fetch_elems.startswith((
-            "audio",
-            )):
+        img = Image.open(thumb_image_path)
+        if cb_data.startswith(("audio", "docaudio", "docvideo")):
             img.resize((320, height))
         else:
             img.resize((90, height))
-        img.save(urljpegclone, "JPEG")
-    if not fetch_elems.startswith((
-        "video",
-        "audio",
-        )):
+        img.save(thumb_image_path, "JPEG")
+     #   print(thumb_image_path)
+    if not cb_data.startswith(("video", "audio", "docaudio")):
         print("no data found")
         raise ContinuePropagation
 
     filext = "%(title)s.%(ext)s"
-    imported_host = os.path.join(os.getcwd(), "downloads", str(data_pod.message.chat.id))
+    userdir = os.path.join(os.getcwd(), "downloads", str(q.message.chat.id))
 
-    if not os.path.isdir(imported_host):
-        os.makedirs(imported_host)
-    await data_pod.edit_message_reply_markup(
-        InlineKeyboardMarkup(
-        [[InlineKeyboardButton(
-        "Getting BEST_QUALITY💋",
-        callback_data="down")
-        ]]))
-    filepath = os.path.join(imported_host, filext)
+    if not os.path.isdir(userdir):
+        os.makedirs(userdir)
+    await q.edit_message_reply_markup(
+        InlineKeyboardMarkup([[InlineKeyboardButton("Getting BEST_QUALITY💋", callback_data="down")]]))
+    filepath = os.path.join(userdir, filext)
+    # await q.edit_message_reply_markup([[InlineKeyboardButton("Processing..")]])
 
     audio_command = [
         "youtube-dl",
-        "-fetch",
+        "-c",
         "--prefer-ffmpeg",
         "--extract-audio",
         "--audio-format", "mp3",
         "--audio-quality", format_id,
         "-o", filepath,
-        fetchedlink,
+        yturl,
+
     ]
+
     video_command = [
         "youtube-dl",
-        "-fetch",
+        "-c",
         "--embed-subs",
         "-f", f"{format_id}+bestaudio",
         "-o", filepath,
-        "--hls-prefer-ffmpeg", fetchedlink]
-    item_spawned = None
-    
-    if fetch_elems.startswith("audio"):
-        filename = await downloadaudiocli(audio_command)
-        item_spawned = InputMediaAudio(
-            media=filename,
-            thumb=urljpegclone,
-            caption=(
-            "ፑ𝐫0ṃ\n@YoutubeDownloadVrtx_Bot📥"
-            ),
-            title=os.path.basename(filename)
-            )
+        "--hls-prefer-ffmpeg", yturl]
 
-    if fetch_elems.startswith("video"):
-        filename = await downloadvideocli(video_command)
-        dur = round(duration(filename))
-        item_spawned = InputMediaVideo(
+    loop = asyncio.get_event_loop()
+
+    med = None
+    if cb_data.startswith("audio"):
+        filename = await downloadaudiocli(audio_command)
+        med = InputMediaAudio(
             media=filename,
-            duration=dur,
+            thumb=thumb_image_path,
+            caption=("ፑ𝐫0ṃ\n@YoutubeDownloadVrtx_Bot📥"),
+            title=os.path.basename(filename)
+        )
+
+    if cb_data.startswith("video"):
+        filename = await downloadvideocli(video_command)
+        #dur = round(duration(filename))
+        med = InputMediaVideo(
+            media=filename,
+            #duration=dur,
             width=width,
             height=height,
-            thumb=urljpegclone,
-            caption=(
-            "ፑ𝐫0ṃ\n@YoutubeDownloadVrtx_Bot📥"
-            ),
+            thumb=thumb_image_path,
+            caption=("ፑ𝐫0ṃ\n@YoutubeDownloadVrtx_Bot📥"),
             supports_streaming=True
         )
 
-    if item_spawned:
-        loop.create_task(ytdata_throw(
-        fetch,
-        data_pod,
-        item_spawned,
-        filename))
-    else:
-        print(
-        "media not found"
+    if cb_data.startswith("docaudio"):
+        filename = await downloadaudiocli(audio_command)
+        med = InputMediaDocument(
+            media=filename,
+            thumb=thumb_image_path,
+            caption=("ፑ𝐫0ṃ\n@YoutubeDownloadVrtx_Bot📥"),
         )
 
-async def ytdata_throw(
-    fetch,
-    data_pod,
-    item_spawned,
-    filename):
-    print(item_spawned)
+    if med:
+        loop.create_task(send_file(c, q, med, filename))
+    else:
+        print("media not found")
+
+async def send_file(c, q, med, filename):
+    print(med)
     try:
-        await data_pod.edit_message_reply_markup(
+        await q.edit_message_reply_markup(
                  InlineKeyboardMarkup([[InlineKeyboardButton("Uploading📤", callback_data="down")]])          
             )
-        await fetch.send_chat_action(chat_id=data_pod.message.chat.id, action="record_video")
-        await data_pod.edit_message_media(media=item_spawned)
+        await c.send_chat_action(chat_id=q.message.chat.id, action="record_video")
+        await q.edit_message_media(media=med)
     except Exception as e:
         print(e)
-        await data_pod.edit_message_text(e)
+        await q.edit_message_text(e)
     finally:
         try:
             os.remove(filename)
-            os.remove(urljpegclone)
+            os.remove(thumb_image_path)
         except:
             pass
