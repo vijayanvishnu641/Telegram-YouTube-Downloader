e600f831e1250b0079e823845070abc39d01ea9d
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-21 15:23:49 +0530

    qwd

--- ɛʟɛʍֆ/importing.py
+++ ɛʟɛʍֆ/importing.py
@@ -16,22 +16,26 @@ os.system(KK)
 from dlhunt.dlhunt import *
 
 @Client.on_callback_query()
-async def catch_youtube_fmtid(c, m):
-    cb_data = m.data
-    if cb_data.startswith("ytdata||"):
-        yturl = cb_data.split("||")[-1]
-        format_id = cb_data.split("||")[-2]
-        media_type = cb_data.split("||")[-3].strip()
+async def catch_youtube_fmtid(
+    _,
+    buta):
+    fetch_elems = buta.data
+    if fetch_elems.startswith("fetchedfile||"):
+        fetchedlink = fetch_elems.split("||")[-1]
+        format_id = fetch_elems.split("||")[-2]
+        media_type = fetch_elems.split("||")[-3].strip()
         print(media_type)
         if media_type == 'audio':
-            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "Audio", callback_data=f"{media_type}||{format_id}||{yturl}"),]])
+            pod = InlineKeyboardMarkup(
+                [[InlineKeyboardButton(
+                "Best-Mp3",
+                callback_data=f"{media_type}||{format_id}||{fetchedlink}")]])
         else:
-            buttons = InlineKeyboardMarkup([[InlineKeyboardButton(
-                "Video", callback_data=f"{media_type}||{format_id}||{yturl}")]])
-
-        await m.edit_message_reply_markup(buttons)
-
+            pod = InlineKeyboardMarkup(
+                [[InlineKeyboardButton(
+                "Best-Mp4",
+                callback_data=f"{media_type}||{format_id}||{fetchedlink}")]])
+        await buta.edit_message_reply_markup(pod)
     else:
         raise ContinuePropagation
 
