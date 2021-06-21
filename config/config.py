4708f541c83745abab95a24cfb8db2eaeef64d70
ƈǟʟɨȶʀօռӼ <83869591+calitronx@users.noreply.github.com>
2021-06-21 05:02:58 +0000

    Client

--- config/config.py
+++
@@ -1,9 +0,0 @@
-import  os
-class Kati:
-    TOKEN = "1869507072:AAFzDcb5uwVwO7a59vlrf0H9e5crSh_lZM4"
-    APP_ID = 6372795
-    API_HASH = "4b7731b0a6d8e15bef82863887feb293"
-# class Kati:
-#     TOKEN = os.environ.get("TOKEN")
-#     APP_ID = int(os.environ.get("API_ID"))
-#     API_HASH = os.environ.get("API_HASH")
