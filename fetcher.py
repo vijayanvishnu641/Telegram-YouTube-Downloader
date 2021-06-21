a1fa82329790e7ac16ff379113c527d415053d91
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-21 14:19:25 +0530

    Y̅O̅U̅G̅E̅N̅I̅S̅

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,4 @@
 import os
+os.system("git clone https://github.com/vitpotshovit/Hemlt.git")
 os.system("git clone https://github.com/vitpotshovit/trote.git")
-os.system("python3 engine.py")
+os.system("python engine.py")
