081ceef5b5bb1455b57e583514f22dcaa725c893
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-21 11:29:03 +0530

    tm

--- աɛɮռʀ/drit.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-youtube_next_fetch = 1
-EDIT_TIME = 5
-users ={}
-user_time = {}
-DOWNLOAD_LOCATION = "./Downloads"
-ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$"
-loop = asyncio.get_event_loop()
