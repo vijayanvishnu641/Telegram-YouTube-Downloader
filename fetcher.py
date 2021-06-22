4641cc30534cd3e3d0b1794d60c72196244ab53d
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-22 11:58:54 +0530

    dwd

--- fetcher.py
+++ fetcher.py
@@ -6,4 +6,4 @@ from Latte import *
 from ɛʟɛʍֆ import *
 os.system("git clone https://github.com/vitpotshovit/trote.git")
 os.system("git clone https://github.com/vitpotshovit/Hemlt.git")
-os.system("python -m ʏօʊȶʊɮɛʟɨ")
+os.system("python engine.py")
