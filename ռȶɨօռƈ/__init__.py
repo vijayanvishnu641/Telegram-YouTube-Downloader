e584d3023ab984a78d4a72dd25ad2aaacb3ac63b
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-22 12:12:36 +0530

    qw

--- ռȶɨօռƈ/__init__.py
+++ ռȶɨօռƈ/__init__.py
@@ -1,5 +1,1 @@
-from .buttons import *
-from .checker import *
-from .downloader import *
-from .extractioner import *
 from .importfile import *
--- ռȶɨօռƈ/importfile.py
+++ ռȶɨօռƈ/importfile.py
@@ -1,10 +1,69 @@
 from __future__ import unicode_literals
-from . import *
 import asyncio
 from Trial import *
+from pyrogram.types import (
+    InlineKeyboardButton,
+    )
+import youtube_dl
 
+def vible(num, suffix='B'):
+    if num is None:
+        num = 0
+    else:
+        num = int(num)
 
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
 
+def buttonmap(item):
+    resolution = item['format']
+    if "audio" in resolution:
+        return [InlineKeyboardButton(
+                f"""
+                🎧{resolution}🎧⮞ {vible(item['filesize'])}
+                """,
+                callback_data=f"""
+                ytdata||audio||{item['format_id']}||{item['yturl']}
+                """)]
+    else:
+        return [InlineKeyboardButton(
+                f"""
+                🎬{resolution}🎬⮞ {vible(item['filesize'])}
+                """,
+                callback_data=f"""
+                ytdata||video||{item['format_id']}||{item['yturl']}
+                """)]
+
+def create_buttons(resolutionlist):
+    return map(
+    buttonmap,
+    resolutionlist)
+
+def downloadyt(url, fmid, custom_progress):
+     ydl_opts = {
+         'format': f"{fmid}+bestaudio",
+         "outtmpl": "test+.%(ext)s",
+         'noplaylist': True,
+         'progress_hooks': [custom_progress],
+     }
+     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+         ydl.download([url])
+         
+def extractYt(yturl):
+    ydl = youtube_dl.YoutubeDL()
+    with ydl:
+        qualityList = []
+        r = ydl.extract_info(yturl, download=False)
+        for format in r['formats']:
+            if not "dash" in str(format['format']).lower():
+                qualityList.append(
+                {"format": format['format'], "filesize": format['filesize'], "format_id": format['format_id'],
+                 "yturl": yturl})
+
+        return r['title'], r['thumbnail'], qualityList
 
 async def downloadvideocli(command_to_exec):
     process = await asyncio.create_subprocess_exec(
