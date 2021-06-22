c393d50c2183a0e8a8e31c9c55894d4f1caed2fb
mastermind_vrtx <mastermindvrtx@gmail.com>
2021-06-22 12:03:07 +0530

    qwd

--- Latte/config.py
+++ Latte/config.py
@@ -4,7 +4,7 @@ class Kryogenesis(object):
     # APP_ID = 6372795
     # TOKEN = '1834680052:AAF0esEckEAujn87y4U3ezCSyIS-x9Dik7E'
     # YOUTUBE_KEY = "TIMXRCTC0RSPYKAEVRTNITVERLER"
-    YOUTUBE_KEY = os.environ.get("YOUGENIS_KEY")
+    YOUTUBE_KEY = os.environ.get("YOUTUBE_KEY")
     TOKEN = os.environ.get("TOKEN")
     APP_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
